6db0fbdf62445a78bd52dd444b3dced1d54754b9
HARUTO SUZUKI <harutosuzuki@HARUTOnoMacBook-Air.local>
2020-08-16 11:59:44 +0900

    forth commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,17 @@
     <link href="/css/app.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>MyQuizMaker</title>
+
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+    <script>
+      window.OneSignal = window.OneSignal || [];
+      OneSignal.push(function() {
+        OneSignal.init({
+          appId: "c27fcc5d-f76e-48a6-99c8-018ecc00c78c",
+        });
+      });
+    </script>
+
   </head>
   <body>
     <body>
